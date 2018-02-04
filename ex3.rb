c97bd660973639db81651cd15bd93110b56045c8
Johnny O'Sullivan <osullivan.johnny@gmail.com>
2018-02-04 19:19:33 +0000

    Exercise 3: Numbers and Math

---
+++ ex3.rb
@@ -0,0 +1,37 @@
+# prints a string with the text between the quotation marks
+puts "I will now count my chickens:"
+
+# prints a string calculates the expression in the braces after the #
+puts "Hens #{25 + 30 / 6}"
+# prints a string calculates the expression in the braces after the #
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# prints a string with the text between the quotation marks
+puts "Now I will count the eggs:"
+
+# prints the result of the calculation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+# prints a string with the text between the quotation marks
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# prints whether the expression is true or false
+puts 3 + 2 < 5 - 7
+
+# prints a string calculates the expression in the braces after the #
+puts "What is 3 + 2? #{3 + 2}"
+# prints a string calculates the expression in the braces after the #
+puts "What is 5 - 7? #{5 - 7}"
+
+# prints a string with the text between the quotation marks
+puts "Oh, that's why it's false."
+
+# prints a string with the text between the quotation marks
+puts "How about some more."
+
+# prints a string and calculates whether the expression is true or false
+puts "Is it greater? #{5 > -2}"
+# prints a string and calculates whether the expression is true or false
+puts "Is it greater or equal? #{5 >= - 2}"
+# prints a string and calculates whether the expression is true or false
+puts "Is it less or equal? #{5 <= -2}"
