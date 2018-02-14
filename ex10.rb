ec8d0f26c34196f0a37fe7cd48312844040a32d7
Johnny O'Sullivan <osullivan.johnny@gmail.com>
2018-02-14 23:01:29 +0000

    Exercise 10: What Was That?

---
+++ ex10.rb
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat="""
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+a_cat = "A new kind of c\rat"
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts a_cat
--- ex9.rb
+++ ex9.rb
@@ -12,3 +12,5 @@ With this weird quote
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 }
+
+puts "jan\nAug"
